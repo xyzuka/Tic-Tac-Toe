41c06c33ece73d0ff44b3ce3e3607fe75669b6b6
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-04-29 22:35:14 +1000

    debugged winning logic

--- script.js
+++ script.js
@@ -13,11 +13,13 @@ window.addEventListener('DOMContentLoaded', () => {
       [6] [7] [8]
   */
 
+  /**********************************/
   // State management
   /**
    * Contain the state and memory storage of the game
    */
   const gameBoard = {
+    _roundWon: false,
     _isGameActive: true,
     _board: ['', '', '', '', '', '', '', '', ''],
     _player1: 'X',
@@ -37,6 +39,11 @@ window.addEventListener('DOMContentLoaded', () => {
     ],
   };
 
+  /**********************************/
+  // Game logic
+  /**
+   * Contains game logic
+   */
   const setCurrentPlayer = (() =>
     (gameBoard._currentPlayer = gameBoard._player1))();
 
@@ -48,36 +55,49 @@ window.addEventListener('DOMContentLoaded', () => {
   };
 
   const addPlayerScore = function (index) {
-    gameBoard._currentPlayer === gameBoard._player1
-      ? gameBoard._player2Score.push(+index)
-      : gameBoard._player1Score.push(+index);
-    console.log(gameBoard._player1Score);
-    console.log(gameBoard._player2Score);
+    // console.log(gameBoard._currentPlayer);
+    // console.log(index);
+    if (gameBoard._currentPlayer === gameBoard._player1) {
+      gameBoard._board[index] = gameBoard._currentPlayer;
+    } else {
+      gameBoard._board[index] = gameBoard._currentPlayer;
+    }
   };
 
   /**
    * Compares player's score to the winning condition's array
    */
   const comparingScores = function (winningCond, playerScore) {
-    const playerScoreString = JSON.stringify(playerScore);
-    console.log(playerScoreString);
+    for (let i = 0; i <= 7; i++) {
+      const winCondition = gameBoard._winningConditions[i];
 
-    const comparingScores = winningCond.some(function (score) {
-      return JSON.stringify(score) === playerScoreString;
-    });
-    console.log(comparingScores);
+      const a = gameBoard._board[winCondition[0]];
+      const b = gameBoard._board[winCondition[1]];
+      const c = gameBoard._board[winCondition[2]];
 
-    return comparingScores;
+      // console.log(`a: ${a}`);
+      // console.log(`b: ${b}`);
+      // console.log(`c: ${c}`);
+
+      if (a === '' || b === '' || c === '') {
+        continue;
+      }
+      if (a === b && b === c) {
+        gameBoard._roundWon = true;
+        break;
+      }
+    }
   };
 
   const checkGameOver = function () {
-    if (comparingScores(gameBoard._winningConditions, gameBoard._player1Score))
-      alert('Player 1 Wins!');
+    comparingScores();
+    if (gameBoard._roundWon) alert('Player Wins!');
 
-    if (comparingScores(gameBoard._winningConditions, gameBoard._player2Score))
-      alert('Player 2 Wins!');
+    // if (comparingScores(gameBoard._winningConditions, gameBoard._player2Score))
+    //   alert('Player 2 Wins!');
   };
 
+  /**********************************/
   // UI - Rendering
   /**
    * Renders tile which player selects
@@ -86,14 +106,14 @@ window.addEventListener('DOMContentLoaded', () => {
     if (tile.textContent === '') {
       tile.textContent = gameBoard._currentPlayer;
       tile.classList.add(`player${gameBoard._currentPlayer}`);
-      console.log(index);
 
-      switchPlayers();
       addPlayerScore(index);
+      switchPlayers();
       checkGameOver();
     }
   };
 
+  /**********************************/
   // Controller - User interactions
   /**
    * Listens for player to click a tile.
